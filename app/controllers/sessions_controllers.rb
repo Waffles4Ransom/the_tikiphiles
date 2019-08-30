8fcfbe57f29a0849c1d43c9f26b8f86131586b3e
Jenna Ransom <jenna.ransom@gmail.com>
2019-08-30 03:33:34 -0600

    set up sign-up to handle error messaging and built out sessions sign-up route

--- app/controllers/sessions_controllers.rb
+++ app/controllers/sessions_controllers.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
 
   get '/signup' do
+    redirect '/drinks' if logged_in?
     erb :'sessions/signup'
   end 
 
   get '/login' do 
+    redirect '/drinks' if logged_in?
     erb :'sessions/login'
   end
 
@@ -17,6 +19,17 @@ class SessionsController < ApplicationController
       @error = "Invalid attempt, please try again."
       erb :'sessions/login'
     end 
+  end 
+
+  post '/signup' do 
+    @user = User.create(name: params[:name], username: params[:username], password: params[:password])
+    if @user.errors.any?
+      erb :'sessions/signup'
+    else
+      session[:user_id] = user.id
+      redirect '/drinks/index'
+    end
+
   end 
 
 end 
