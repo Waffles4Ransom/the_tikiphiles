bc9c4da4e8c459743949392cc03545ffd2eb62cf
Jenna Ransom <jenna.ransom@gmail.com>
2019-08-29 23:48:29 -0600

    add user validations and login route, fix minor errors

--- app/controllers/sessions_controllers.rb
+++ app/controllers/sessions_controllers.rb
@@ -8,4 +8,9 @@ class SessionsController < ApplicationController
     erb :'sessions/login'
   end
 
+  post '/login' do 
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id
+  end 
+
 end 
