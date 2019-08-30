4c75d5ae41872e1d91f3ce69a1ce98eeddf9c992
Jenna Ransom <jenna.ransom@gmail.com>
2019-08-30 01:00:40 -0600

    add new view, update sessions routes, add css

--- app/controllers/sessions_controllers.rb
+++ app/controllers/sessions_controllers.rb
@@ -10,7 +10,13 @@ class SessionsController < ApplicationController
 
   post '/login' do 
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id
+    if !!user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/drinks/index'
+    else 
+      @error = "Invalid attempt, please try again."
+      erb :'sessions/login'
+    end 
   end 
 
 end 
